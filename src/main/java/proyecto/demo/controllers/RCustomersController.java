24ea279c6d068a652ec0c32bc39e2c2291acf46f
Cristian Alcala <cristian.alcala@uabc.edu.mx>
2022-07-25 13:19:00 -0700

    timeleaf html customer

--- src/main/java/proyecto/demo/controllers/RCustomersController.java
+++ src/main/java/proyecto/demo/controllers/RCustomersController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import proyecto.demo.entities.Customer;
+
 @Controller
 public class RCustomersController {
 
@@ -15,6 +17,7 @@ public class RCustomersController {
 public String ObtenerVistaRegistro(@RequestParam(value="error", required = false) String error, Model model, Principal principal) {
 	if (error != null) 
 		model.addAttribute("error","Escribio algun dato incorrecto");
+		model.addAttribute("Cliente", new Customer());
 		
 	return "views/registroClientes";
 	}
